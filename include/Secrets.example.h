5096df93e7c0ea228324a7eaf068373bde0c847d
Kevin <kt4bn1@gmail.com>
2026-07-12 16:46:58 -0400

    first commit

---
+++ include/Secrets.example.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Cellular
+#define APN "hologram"
+#define APN_USER ""
+#define APN_PASS ""
+
+// MQTT
+#define MQTT_HOST "YOUR_MQTT_BROKER"
+#define MQTT_PORT 8883
+
+#define MQTT_USER "camper_monitor"
+#define MQTT_PASSWORD "YOU_PASSWORD"
+
+#define MQTT_CLIENT_ID "camper-monitor-01"
